fba90f50d8680627d3fec1b3df9525147b178d5c
aliouswang <wsw622@126.com>
2017-09-20 18:12:02 +0800

    init commit

--- lib/src/main/java/com/example/aliouswang/lib/Picasso_.java
+++ lib/src/main/java/com/example/aliouswang/lib/Picasso_.java
@@ -3,6 +3,9 @@ package com.example.aliouswang.lib;
 import android.net.Uri;
 
 /**
+ *
+ * http://blog.csdn.net/u012702547/article/details/52273918
+ *
  * Created by aliouswang on 2017/9/20.
  */
 
